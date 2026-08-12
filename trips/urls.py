75762b896f863e9d212cd1268e69c84a2d83c2a6
al23029 <al23029@shibaura-it.ac.jp>
2026-08-12 17:33:28 +0700

    feat(frontend): add templates and basic css

--- trips/urls.py
+++ trips/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 # path("login/", views.login_view, name="login")
 # This means if the url is ~/login/, go check views.py -> def login_view
@@ -9,8 +10,39 @@ urlpatterns = [
     path("", views.landing, name="landing"),
 
     path("login/", views.login_view, name="login"),
-    path("forgot-password/", views.forgot_password, name="forgot_password"),
     path("signup/", views.signup, name="signup"),
+
+    path(
+        "forgot-password/",
+        auth_views.PasswordResetView.as_view(
+            template_name="trips/forgot_password.html"
+        ),
+        name="forgot_password",
+    ),
+
+    path(
+        "forgot-password/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="trips/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="trips/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="trips/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 
     path("logout/", LogoutView.as_view(), name="logout"),
 
